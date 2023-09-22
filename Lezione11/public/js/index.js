28e653dd2f125eab8ce3efead8a6e8fdfa22cac4
cyberdelia-dev <cyberdeliaeu@gmail.com>
2023-09-22 21:29:49 +0200

    garage search

--- Lezione11/public/js/index.js
+++ Lezione11/public/js/index.js
@@ -1,50 +1,48 @@
 
-function createArticle(title, content) {
 
-    const articleElement = document.createElement('article');
-
-
-    const now = new Date();
-    const dateElement = document.createElement('span');
-    dateElement.textContent = now.toLocaleDateString() + ' alle ' + now.toLocaleTimeString();
-    articleElement.appendChild(dateElement);
-
-    const titleElement = document.createElement('h2');
-    titleElement.textContent = title;
-    articleElement.appendChild(titleElement);       // has-a
-
-    const contentElement = document.createElement('p');
-    contentElement.textContent = content;
-    articleElement.appendChild(contentElement);
-
-    return articleElement;
-}
+const garage = [
+    {
+        brand: 'fiat',
+        model: '500'
+    },
+    {
+        brand: 'fiat',
+        model: 'punto'
+    },
+    {
+        brand: 'ferrari',
+        model: '512'
+    }
+];
 
 
+const foundList = document.getElementById('foundList');
 
-const main = document.querySelector('main');
+const brandInput = document.getElementById('brandInput');
+const searchForm = document.getElementById('searchForm');
+searchForm.addEventListener('submit', (event) => {
 
-const titleInput = document.getElementById('titleInput');
-const contentTextarea = document.getElementById('contentTextarea');
-
-const articleForm = document.getElementById('articleForm');
-articleForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const article = createArticle(titleInput.value, contentTextarea.value);
-    main.appendChild(article);
+    //foundList.innerHTML = '';
+    // rimuovo i figli di foundList
+    while(foundList.hasChildNodes()) {
+        const firstChild = foundList.firstChild;
+        foundList.removeChild(firstChild);
+    }
+
+
+    garage.forEach((car) => {
+        if(car.brand == brandInput.value) {
+
+            const li = document.createElement('li');
+            li.textContent = car.model;
+    
+            foundList.appendChild(li);
+        }
+    });
+
+    brandInput.value = '';
 });
-
-
-// unix timestamp
-// millisecondi trascorsi dal 1 gennaio 1970 UTC
-
-// i mesi partono da 0 dove 0 è gennaio mentre 11 è dicembre
-// getDate() il giorno del mese
-// getDay() il giorno della settimana
-
-
-
-
 
 
